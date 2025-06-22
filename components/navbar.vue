4ac9704d2c7eee62b2fce2e35c10a4fea4b5534c
AMRWERY <amrmounir2@gmail.com>
2025-06-22 23:03:34 +0300

    finish handling navbar design

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,62 +1,64 @@
 <template>
   <div>
-    <header
-      class="flex py-4 px-4 sm:px-6 bg-white min-h-[75px] tracking-wide relative z-50">
-      <div class="flex flex-wrap items-center gap-5 w-full max-w-screen-xl mx-auto">
+    <header class="flex py-4 px-4 md:px-[60px] sm:px-6 bg-white min-h-[101px] tracking-wide relative z-50">
+      <div class="flex flex-wrap items-center gap-5 w-full mx-auto">
         <nuxt-link to="">
           <img src="/public/toyota-logo.jpg" alt="toyota-logo" class="w-[144.76px] h-[47.82px]" />
         </nuxt-link>
 
-        <div id="collapseMenu"
-          class="max-lg:hidden lg:!flex lg:ml-auto max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50">
-          <button id="toggleClose"
+        <div id="collapseMenu" v-show="isMenuOpen" :class="[
+          'lg:!flex lg:ml-auto max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50',
+          isMenuOpen ? 'max-lg:block' : 'max-lg:hidden'
+        ]">
+          <button id="toggleClose" @click="isMenuOpen = false"
             class="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white w-9 h-9 flex items-center justify-center border border-gray-200 cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-3.5 h-3.5 fill-black" viewBox="0 0 320.591 320.591">
-              <path
-                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-                data-original="#000000"></path>
-              <path
-                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-                data-original="#000000"></path>
-            </svg>
+            <icon name="material-symbols:close-small-rounded" class="text-black" />
           </button>
 
           <ul
-            class="lg:flex gap-4 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
+            class="lg:flex gap-12 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
             <li class="mb-6 hidden max-lg:block">
               <nuxt-link to="">
                 <img src="/public/toyota-logo.jpg" alt="toyota-logo" class="w-[144.76px] h-[47.82px]" />
               </nuxt-link>
             </li>
             <li class="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-              <nuxt-link to="" class="hover:text-blue-700 text-blue-700 font-medium block text-[15px] uppercase">Products</nuxt-link>
+              <nuxt-link to=""
+                class="hover:text-blue-700 text-[#282830] font-400 block text-lg uppercase cursor-pointer">Products</nuxt-link>
             </li>
             <li class="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
               <nuxt-link to=""
-                class="hover:text-blue-700 text-slate-900 font-medium block text-[15px] uppercase">Services</nuxt-link>
+                class="hover:text-blue-700 text-[#282830] font-400 block text-lg uppercase cursor-pointer">Services</nuxt-link>
             </li>
             <li class="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
               <nuxt-link to=""
-                class="hover:text-blue-700 text-slate-900 font-medium block text-[15px] uppercase">Training</nuxt-link>
+                class="hover:text-blue-700 text-[#282830] font-400 block text-lg uppercase cursor-pointer">Training</nuxt-link>
             </li>
             <li class="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
               <nuxt-link to=""
-                class="hover:text-blue-700 text-slate-900 font-medium block text-[15px] uppercase">About Us</nuxt-link>
+                class="hover:text-blue-700 text-[#282830] font-400 block text-lg uppercase cursor-pointer">About
+                Us</nuxt-link>
+            </li>
+            <li class="max-lg:py-3 px-3 flex md:mr-[65px]">
+              <icon name="iconamoon:search" class="w-[18px] h-[18px] hidden lg:block mt-1" />
+              <div v-if="isMenuOpen" class="lg:hidden relative w-full">
+                <input type="text" v-model="searchQuery" placeholder="Search..."
+                  class="w-full py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-700" />
+                <icon name="iconamoon:search"
+                  class="w-[18px] h-[18px] absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+              </div>
             </li>
           </ul>
         </div>
 
         <div class="flex items-center max-lg:ml-auto space-x-4">
           <button
-            class="px-4 py-2 text-[15px] rounded-sm font-medium text-white bg-blue-600 hover:bg-blue-700 cursor-pointer">Sign
-            up</button>
+            class="text-[15px] rounded-[18px] font-medium text-white bg-[#CE190A] hover:bg-red-600 cursor-pointer w-[136px] h-[48px]">Get
+            Quote
+          </button>
 
-          <button id="toggleOpen" class="lg:hidden cursor-pointer">
-            <svg class="w-7 h-7" fill="#333" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"></path>
-            </svg>
+          <button id="toggleOpen" class="lg:hidden cursor-pointer" @click="isMenuOpen = true">
+            <icon name="material-symbols:menu" class="w-7 h-7" />
           </button>
         </div>
       </div>
@@ -65,5 +67,6 @@
 </template>
 
 <script lang="ts" setup>
-
+const isMenuOpen = ref(false)
+const searchQuery = ref('')
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,11 @@
     <!-- navbar component -->
     <navbar />
 
-    <slot />
+    <div class="max-w-full mx-auto px-2 sm:px-6 lg:px-[60px]">
+      <main>
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
 
