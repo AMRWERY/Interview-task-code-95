dedae556178866b5b28ecb11c89146a5afd3b171
AMRWERY <amrmounir2@gmail.com>
2025-06-23 18:03:08 +0300

    handle create product-cards-skeleton.vue component and use it when fetch data

---
+++ components/product-cards-skeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="bg-white rounded-xl custom-shadow py-5 text-center w-[326px] min-h-[390px] gap-2.5 animate-pulse">
+      <div class="w-[195.93px] h-[195.93px] bg-gray-200 mx-auto rounded"></div>
+
+      <div class="mt-5 h-4 w-3/4 bg-gray-200 mx-auto rounded"></div>
+      <div class="mt-3 h-3 w-1/2 bg-gray-200 mx-auto rounded"></div>
+
+      <div class="mt-3 h-3 w-[70%] bg-gray-200 mx-auto rounded"></div>
+
+      <div class="mt-4 h-[48px] w-[161px] bg-gray-200 mx-auto rounded-[18px]"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.custom-shadow {
+  box-shadow: 0px 0px 4px 0px #00000059;
+}
+</style>
--- components/product-cards.vue
+++ components/product-cards.vue
@@ -4,18 +4,24 @@
       <div class="container mx-auto">
         <div
           :class="['grid', cards.length === 1 ? 'place-items-center' : 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-4', 'gap-[30px]']">
-          <div v-for="card in cards" :key="card.id"
-            class="bg-white rounded-xl custom-shadow py-5 text-center w-[326px] min-h-[390px] gap-2.5">
-            <img :src="card.featured_image" alt="product" class="w-[195.93px] h-[195.93px] object-contain mx-auto">
-            <h3 class="text-base font-500 text-[#CE190A] mt-5 uppercase">{{ card.name }}</h3>
-            <p class="text-[#282830] mt-2.5 font-500 text-sm">{{ card.code }}</p>
-            <p class="text-[#282830] mt-2.5 font-500 text-sm uppercase">capacity: <span class="text-[#CE190A]">{{
-              getCapacity(card)
-                }}</span></p>
+          <template v-if="productsStore.isLoading">
+            <product-cards-skeleton v-for="n in 4" :key="n" />
+          </template>
 
-            <button
-              class="mt-4 bg-[#CE190A] text-white w-[161px] h-[48px] rounded-[18px] hover:bg-red-600 uppercase text-sm font-700">viewproduct</button>
-          </div>
+          <template v-else>
+            <div v-for="card in cards" :key="card.id"
+              class="bg-white rounded-xl custom-shadow py-5 text-center w-[326px] min-h-[390px] gap-2.5">
+              <img :src="card.featured_image" alt="product" class="w-[195.93px] h-[195.93px] object-contain mx-auto">
+              <h3 class="text-base font-500 text-[#CE190A] mt-5 uppercase">{{ card.name }}</h3>
+              <p class="text-[#282830] mt-2.5 font-500 text-sm">{{ card.code }}</p>
+              <p class="text-[#282830] mt-2.5 font-500 text-sm uppercase">capacity: <span class="text-[#CE190A]">{{
+                getCapacity(card)
+                  }}</span></p>
+
+              <button
+                class="mt-4 bg-[#CE190A] text-white w-[161px] h-[48px] rounded-[18px] hover:bg-red-600 uppercase text-sm font-700">viewproduct</button>
+            </div>
+          </template>
         </div>
       </div>
     </div>
